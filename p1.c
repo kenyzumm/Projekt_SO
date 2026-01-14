c23c714cfb5962acf473a2171f17cef56c312d01
OliwierFi <fijalkowskioliwier@gmail.com>
2026-01-14 23:32:47 +0100

    Create p1.c

---
+++ p1.c
@@ -0,0 +1,14 @@
+ pid1 = fork();
+    if(pid1 == 0) {
+        // Kod P1 - konsument
+
+        signal(SIGUSR2, sig_handler); // p1  zeka na sygnal od p2 
+        while(1) {
+         
+            P.sem_num = 0; semop(semid, &P, 1);                //p1 czeka na semafor
+            printf("P1 odczytuje liczbę: %d\n", *shared_data);  //p1 czyta liczbe znakow z wspolniej pamieci i ja wypisuje
+            V.sem_num = 0; semop(semid, &V, 1);   //ustawia semafor na 0(zwalnia go), p1 sie blokuje
+            
+        }
+        exit(0);
+    }

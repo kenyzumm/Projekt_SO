016871b7e83f47c6d867367708b5266c2e2eaeb8
Daniel J <danieljaskulowski@gmail.com>
2026-01-15 10:36:00 +0100

    Definicja struktury shared_data i kluczy IPC

--- ipc.h
+++ ipc.h
@@ -2,10 +2,22 @@
 #define IPC_H
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
 
 #define KEY_PATH "ipc.key"
 #define KEY ftok(KEY_PATH, 'A')
 #define MAX_LENGTH 1024
+
+#define SEM_KEY 5678L
+#define SHM_KEY 1234L
 
 extern int debug_mode;
 
@@ -17,10 +29,10 @@ void proc3();
 
 void handle_error(const char* msg, int exit_code);
 void debug(const char* msg);
+void sem_op(int semid, int sem_num, int op);
 
-#endif // IPC_H
-       //
-       //
-       // TEST
-       //
-       // dalsze testy
+struct shared_data {
+    char text[256]; // Bufor na tekst przesyłany między procesami
+};
+
+#endif
